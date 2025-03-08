a1473ced3a6fcb9ffd6db28b545277357c1201ab
claytakiler <claytonhouser10@gmail.com>
2025-03-08 14:22:54 -0600

    Finished Object Class

--- src/shared/Utilities/Object/Object.lua
+++ src/shared/Utilities/Object/Object.lua
@@ -2,16 +2,27 @@
 This module script provides the basis for all inheritince.
 All base classes MUST inherit from this.
 In addition, it provides multiple utilities for an Object below.
+All objects inherit these utilities.
 --]]
-local Object = {}
-Object.__index = Object
+local Object : any = {}
+Object.__index = Object --Set Object has lookup.
 
+--[[
+Base constructor of all Objects
+    @param Name (string) required name of the instance you are creating 
+    @return (instance) instance of the object.
+--]]
 function Object.new(Name: string) : any
     local self: any = setmetatable({}, Object)
     self.Name = Name or error("Class did not define \"Name\" when instantiated", 5)
     return self
 end
 
+--[[
+Helper function that recursively copys all elements and its children
+    @param Instance (instance) any Object instance
+    @return (instance) copy of instance and a fresh copy of all its children 
+--]]
 local function NestedCopy(Instance: any) : any
     if type(Instance) ~= "table" then
         --Not table, recursive copy not needed.
@@ -26,6 +37,12 @@ local function NestedCopy(Instance: any) : any
     end
 end
 
+--[[
+Determines if an object is an instance of another object
+    @param Instance (instance) the instance to check for
+    @param Parent (object) object asumed to be inherited from
+    @return (boolean) returns true if is instance or false otherwise
+--]]
 function Object:IsInstance(Instance: any, Parent: any) : boolean
     local metaTable: any = getmetatable(Instance)
 
@@ -34,11 +51,17 @@ function Object:IsInstance(Instance: any, Parent: any) : boolean
         if metaTable == Parent then
             return true--Is instance of parent
         end
-        metaTable = getmetatable(metaTable)
+        metaTable = getmetatable(metaTable)--Traverse table
     end
     return false --Not instance of parent
 end
 
+--[[
+Determines if an instance is of  a specific object type
+    @param Instance (instance) the instance to check for being of that type
+    @param Object (object) the object type asumed to be the same as instance
+    @return (boolean) returns true if instance is of same object type or false otherwise
+--]]
 function Object:IsObject(Instance: any, Object: any) : boolean
     local metaTable: any = getmetatable(Instance)
     if metaTable == Object then
@@ -48,13 +71,20 @@ function Object:IsObject(Instance: any, Object: any) : boolean
     end
 end
 
+--[[
+Casts a given instance into another object type and returns an entirely new instance
+    any fields/methods not present in the object catsed to is removed
+    any fields that are present in both recieves a one to one copy
+    all table fields do not maintain a pass by refrence
+    @param Instance (instance) instance of an object
+    @param Cast (object) object type to be casted to
+    @return (instance) new instance copy that is casted into the type of Cast
+--]]
 function Object:TypeCast(Instance: any, Cast: any) : any
     if Object:IsInstance(Instance, Cast) then
         local castedInstance: any = Cast.new(Instance.Name)
 
         --Preserve the fields of previous instance if consistent with super class
-        --local currentTable = getmetatable(Instance)
-
         for key, value in pairs(Instance) do
             if castedInstance[key] ~= nil then
                 castedInstance[key] = NestedCopy(value) 
@@ -62,6 +92,7 @@ function Object:TypeCast(Instance: any, Cast: any) : any
         end
         return castedInstance
     else
+        --Not instance and may not cast
         if (Instance.Name and Cast.Name) then
             error("Instance \"" .. Instance.Name .. "\" is not an instance of object \"" .. Cast.Name .. "\" and may not use TypeCast()", 5)
         else
@@ -70,6 +101,12 @@ function Object:TypeCast(Instance: any, Cast: any) : any
     end
 end
 
+--[[
+Checks if two instances have equal names and state. Must be a one to one copy.
+    @param InstanceA (instance) first instance for checking against InstanceB
+    @param InstanceB (instance) second instance for checking against InstanceA
+    @return (boolean) true if one to one copies or false otherwise
+--]]
 function Object:Equal(InstanceA: any, InstanceB: any) : boolean
     --Check if keys in instance a are in instance b/have same value
     for key, value in pairs(InstanceA) do
@@ -92,7 +129,12 @@ function Object:Equal(InstanceA: any, InstanceB: any) : boolean
     return true--All keys and values match
 end
 
-function Object:Supersedes(Class: any)
+--[[
+Method for inheriting from the object (parent) who calls Supersedes
+    Similiar to extends in Java but reversed.
+    @param Class (object) class that is extending the parent class
+--]]
+function Object:Supersedes(Class: any) : ()
     Class.__index = Class
     setmetatable(Class, self)
 end
