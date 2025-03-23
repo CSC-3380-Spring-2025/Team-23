8a451ff599438a0b5a3608132c0b738e48c625e3
Justin <jruss83@lsu.edu>
2025-03-22 22:14:40 -0500

    Finished ToolHandler construction, added new functionality to BackpackHandler to support ToolHandler, created Axe example subclass for tool, fixed some out of guideline code in BackpackHandler.

---
+++ src/server/Player/ItemData.lua
@@ -0,0 +1,12 @@
+local ItemData = {
+    ["Coal"] = {
+        ["MaxStack"] = 50;
+        ["Weight"] = 2;
+    };
+    ["Axe"] = {
+        ["MaxStack"] = 50;
+        ["Weight"] = 10;
+    };
+}
+
+return ItemData
