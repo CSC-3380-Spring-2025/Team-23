86d27d573c320e7d6c4e28b0d5ecfa27f6bf634d
g34rmeister <163631010+g34rmeister@users.noreply.github.com>
2025-03-29 19:59:35 -0500

    Completed proper documentation with comments

--- src/shared/NPC/NPCDialogue.lua
+++ src/shared/NPC/NPCDialogue.lua
@@ -1,3 +1,9 @@
+--[[
+This class provides utilities for establishing, adding, binding to functions,
+removing, closing, and destroying of dialogue elements/GUIs. It is designed to be
+modular with all parameters customizable to the user/developer.
+--]]
+
 --establishing local variables for game services and folders that will need to be accessed
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ReplicatedFirst = game:GetService("ReplicatedFirst")
@@ -11,15 +17,20 @@ Object:Supersedes(NPCDialogue) --this extends Dialogue from Object
 local dialogue = ReplicatedFirst.UI.NPC.Dialogue.Dialogue
 local optionButtonReferencer = ReplicatedFirst.UI.NPC.Dialogue.OptionButton
 
-
+--[[
+Helper function assisting with resetting Proximity Prompt.
+    @param Self (any) - applies to any self reference being utilized in the program.
+    @returns nothing
+--]]
 local function ProxPromptHandler(Self) 
-    --helper function that handles resetting of Proximity Prompt
-    --disconnect actions and reassign them to open new menus afterwards
+    
     local proxPrompt = Self.__ProxPrompt
     local action = Self.__ProxPromptAction
+    --disconnect actions 
     if action then
         action:Disconnect()
     end
+    --and reassign them to open new menus afterwards
     local function NewAction()
         Self.__HomeMenu.Enabled = true
     end
@@ -27,10 +38,12 @@ local function ProxPromptHandler(Self)
 end
 
 
-
+--[[
+Constructor of a new NPC Dialogue
+    @param Name (String) name of the NPC dialogue
+    @param NPC (String) name of the NPC being assigned to that dialogue
+--]]
 function NPCDialogue.new(Name, NPC) 
-    --takes in two parameters, Name and NPC
-    --creates from there a new object with name Name and assigned to NPC
     local self = Object.new(Name)
     setmetatable(self, NPCDialogue) 
     self.__NPC = NPC 
@@ -49,33 +62,37 @@ function NPCDialogue.new(Name, NPC)
 end
 
 
-
+--[[
+Function to insert a new Menu inside NPC Dialogue
+    @param MenuName (String) name of the menu that needs insertion
+    @param Dialogue (String) the content, text of the dialogue being inserted
+--]]
 function NPCDialogue:InsertMenu(MenuName, Dialogue)
-    --takes in MenuName and Dialogue
     --creates a clone of the existing dialogue
-    --modifies it with new text parameters from Dialogue
-    --reinsert it back into player's interface
-    --save to the current Menu table
-    --close the current dialogue
     local dialogueClone = dialogue:Clone()
     dialogueClone.Name = MenuName
+    --modifies its text label with new text parameters from Dialogue
     local cloneTextLabel = dialogueClone.Frame.DialogueFrame.TextLabel
     cloneTextLabel.Text = Dialogue
+    --reinsert it back into player's interface
     dialogueClone.Parent = player.PlayerGui
     dialogueClone.Enabled = false
-    
+    --save to the current Menu table
     self.__Menus[MenuName] = dialogueClone
+    --close the current dialogue
     local function callCloseDialogue()
         self:CloseDialogue()
     end
     --inserts a default close dialogue option for users to easily close out
     self:InsertOption(MenuName, "Nevermind", callCloseDialogue, 10)
-
-
 end
 
+--[[
+Function to set home Menu inside NPC Dialogue
+    @param MenuName (String) name of the menu that needs to be set to Home Menu
+--]]
+
 function NPCDialogue:SetHomeMenu(MenuName)
-    --take in MenuName and set it as the home menu
     local foundMenu = self.__Menus[MenuName]
     --warning when there is not a menu to be set
     if not foundMenu then
@@ -87,49 +104,61 @@ function NPCDialogue:SetHomeMenu(MenuName)
     ProxPromptHandler(self)
 end   
 
+--[[
+Function to insert Option inside a Menu within NPC Dialogue
+    @param MenuName (String) name of the destination menu for the inserted message
+    @param OptionMessage (String) the message being inserted
+    @param ActionFunc (Function) a user-defined function to be binded with the inserted message
+    @param Priority (number) the order in which the inserted message appears in the UI
+--]]
 function NPCDialogue:InsertOption(MenuName, OptionMessage, ActionFunc, Priority)
-    --take in MenuName, OptionMessage, ActionFunc, Priority
+    
     --look up MenuName in the Menus table and save to UI variable
-    --insert new OptionMessage into that UI
-    --set click detector to initiate ActionFunc
-    --set LayoutOrder to Priority (order in which the option message will be showed)
     local currentMenu = self.__Menus[MenuName]
     local optionButtonCopy = optionButtonReferencer:Clone()
+    --set LayoutOrder to Priority (order in which the option message will be showed)
     optionButtonCopy.LayoutOrder = Priority
 
     local scrollingFrame = currentMenu.Frame.OptionFrame.ScrollingFrame
+    --insert new OptionMessage into that UI
     local obrText = optionButtonCopy.TextLabel
     obrText.Text = OptionMessage
+     --set click detector to initiate ActionFunc
     optionButtonCopy.MouseButton1Down:Connect(ActionFunc)
 
     optionButtonCopy.Parent = scrollingFrame
 
 end
-
+--[[
+Function to transition between a menu to another within NPC Dialogue
+    @param MenuName (String) name of the target menu to transition to
+--]]
 function NPCDialogue:TransitionDialogue(MenuName)
     --take in MenuName and set as newMenu
-    --check if newMenu exists, if not, warn
-    --if does, disable CurrentMenu, and enable newMenu
     local newMenu = self.__Menus[MenuName]
+    --check if newMenu exists, if not, warn
     if not newMenu then
         warn("Failed to transition dialogue. " .. MenuName .. " does not exist.")
         return
     end
+    --if does, disable CurrentMenu
     if self.__CurrentMenu then
         self.__CurrentMenu.Enabled = false
     end
+    --and enable newMenu
     newMenu.Enabled = true
     self.__CurrentMenu = newMenu
     
 end
-
+--[[
+Function to close dialogue
+--]]
 function NPCDialogue:CloseDialogue()
-    --closes the dialogue
-    --resets menu to HomeMenu
+    --disables CurrentMenu
     if self.__CurrentMenu then
         self.__CurrentMenu.Enabled = false
     end
-
+    --resets menu to HomeMenu
     if self.__HomeMenu then
         self.__CurrentMenu = self.__HomeMenu
     end
@@ -137,15 +166,15 @@ function NPCDialogue:CloseDialogue()
     ProxPromptHandler(self)
 
 end
-
+--[[
+Function to destroy dialogue instance within NPC Dialogue
+--]]
 function NPCDialogue:DestroyInstance()
-    --destroy NPC dialogue
-    --destroy proximity prompt
-    --destroy Menu table
     print("Destroyed!")
-    
+    --destroy proximity prompt
     self.__ProxPrompt:Destroy()
     print("ProxPrompt is destroyed.")
+    --destroy Menu table
     for _, currentMenu in pairs(self.__Menus) do
          currentMenu:Destroy()
          print("Menu is destroyed.")
