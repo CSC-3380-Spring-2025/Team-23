d38fa712cbe54fcc4e5c097197cbd9b53919de79
Justin <jruss83@lsu.edu>
2025-03-29 23:14:37 -0500

    Updated scripts to align with coding standards.

--- src/server/Player/ItemData.lua
+++ src/server/Player/ItemData.lua
@@ -1,10 +1,18 @@
+--[[  
+* THIS IS A MANIFEST, NOT A CLASS
+* This module stores a table that contains the data for items created in the "backpackhandler" class. Namely, their maximum stack size
+* and weight.
+* All item data containers should have the same parameters, even if their values are 0.
+]]
+
+
 local ItemData = {
     ["Coal"] = {
         ["MaxStack"] = 50;
         ["Weight"] = 2;
     };
     ["Axe"] = {
-        ["MaxStack"] = 50;
+        ["MaxStack"] = 1;
         ["Weight"] = 10;
     };
 }
