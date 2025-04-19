37bc92fdb7bc9947d3e1655dd0ad180cb299240b
claytakiler <claytonhouser10@gmail.com>
2025-04-18 19:16:10 -0500

    Adding thirst handler for statshandler

--- src/client/StarterCharacter/StatsHandler.client.lua
+++ src/client/StarterCharacter/StatsHandler.client.lua
@@ -22,14 +22,14 @@ local statsConfig = {
 	--Handles config for stats
 	MaxFood = 100, --Max hunger of the player
 	MaxHydration = 100, --MaxHydration of the player
-	FdDeteriorationRate = 5, --time in seconds between when food stat gos down
-	HydDeteriorationRate = 120, --time in seconds between when hydration stat gos down
+	FdDeteriorationRate = 60, --time in seconds between when food stat gos down
+	HydDeteriorationRate = 5, --time in seconds between when hydration stat gos down
 	FdDecrement = 20, --The amount that the food stat is decremented by every FdDeteriorationRate
-	HydDecrement = 10, --The amount that the hydration stat is decremented by every HydDeteriorationRate
+	HydDecrement = 20, --The amount that the hydration stat is decremented by every HydDeteriorationRate
 	StarveDmg = 20, --The damage done to a player every StarveDmgRate
 	StarveDmgRate = 5, --rate in seconds that damage is dealt during a starve
-	ThirstDmg = 5, --The damage done to a player every ThirstDmgRate
-	ThirstDmgRate = 30,--rate in seconds that damage is dealt during a thirst
+	ThirstDmg = 20, --The damage done to a player every ThirstDmgRate
+	ThirstDmgRate = 5,--rate in seconds that damage is dealt during a thirst
 }
 
 local stats = {
@@ -80,11 +80,59 @@ tasks.Hunger = task.spawn(function()
         end
 		stats.Food = newStat
 		--Check if starved
-		if newStat <= 0 and not tasks.StvTask then
+		if newStat <= 0 then
 			--Start damaging player and store task to cancel when given food
 			Starve()
 		end
         lastStat = newStat --Update lastStat
         print("Hunger stat is now: " .. stats.Food)
+	end
+end)
+
+--[[
+Starves the player until it is given food
+--]]
+local function Thirst() : ()
+    print("Player is thirsting!")
+	tasks.ThirstTask = task.spawn(function()
+		--Damage NPC
+		while true do
+			if stats.Hydration > 0 then
+                print("Canceling thirst. No longer thirsting!")
+				tasks.ThirstTask = nil
+				return--No longer starving
+			end
+			--Tell server to damage player by given amount
+            StatsDmgPlayer:Fire(statsConfig.ThirstDmg)
+			task.wait(statsConfig.ThirstDmgRate)
+		end
+	end)
+end
+
+--Handle hydration stats
+tasks.Hydration = task.spawn(function()
+    local lastStat = 0
+	while true do
+		task.wait(statsConfig.HydDeteriorationRate) --Wait between decrements
+		local newStat: number = stats.Hydration - statsConfig.HydDecrement
+		if newStat < 0 then
+			newStat = 0 --Prevent negative stat
+		end
+
+        if newStat == lastStat then
+            continue--Skip to next loop nothing new to do
+        else
+            --Update UI for new Hydration stat
+            local newPrcnt = (newStat / statsConfig.MaxHydration)
+            statsUIHandler:AdjustBarUI("Water", newPrcnt, false)
+        end
+		stats.Hydration = newStat
+		--Check if starved
+		if newStat <= 0 then
+			--Start damaging player because of thirst
+			Thirst()
+		end
+        lastStat = newStat --Update lastStat
+        print("Hydration stat is now: " .. stats.Hydration)
 	end
 end)
