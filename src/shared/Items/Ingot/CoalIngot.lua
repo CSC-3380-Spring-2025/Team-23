a8a29954907738ccc375a8c3d3906425d096cbca
Malik <m.malik62205@gmail.com>
2025-04-26 22:49:40 -0500

    Created Spend resources to build and building requirments to check if player has enough gold to place building down. Some furute implementation may involve creating a class for using other item materials like wheat and iron to finalize the building once you spend the gold to place down the schematic. All of the costs for making buildings will be in the building requirements table and each building in replicated storage/buildings should be in it. Finally, I Started the building handler script for handlign client and server communication when it comes to item hadnlign and storage which are server side

---
+++ src/shared/Items/Ingot/CoalIngot.lua
@@ -0,0 +1,8 @@
+local CoalIngot = {}
+CoalIngot.ItemName = "CoalIngot"
+CoalIngot.ItemWeight = 2 -- Weight per 1 item
+CoalIngot.ItemStack = 50 -- Max stack of items possible
+CoalIngot.ItemType = "Ingot"
+CoalIngot.DropItem = nil
+
+return CoalIngot
---
+++ src/shared/Items/Ingot/IronIngot.lua
@@ -0,0 +1,8 @@
+local IronIngot = {}
+IronIngot.ItemName = "IronIngot"
+IronIngot.ItemWeight = 2 -- Weight per 1 item
+IronIngot.ItemStack = 50 -- Max stack of items possible
+IronIngot.ItemType = "Ingot"
+IronIngot.DropItem = nil
+
+return IronIngot
