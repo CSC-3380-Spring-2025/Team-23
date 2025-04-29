d71667452d68ed5ab7d4fd7668d161262fbc5037
claytakiler <claytonhouser10@gmail.com>
2025-04-29 17:06:52 -0500

    Adding events for StatsHandler and fixing race conditions using Mutex locks.

--- src/client/StarterCharacter/StatsHandler.client.lua
+++ src/client/StarterCharacter/StatsHandler.client.lua
@@ -2,21 +2,28 @@
 This script is reserved for handling the stats of a player when their character loads in
 If the player is joining for the first time then the stats info may need to be loaded in first
 All subsequent deaths after a player joins will have an entirely new stats for Hunger and hydration
+DANGER: This script sues Mutex Locks. All exit paths MUST ensure that locks are released when used.
+If a related script pauses execution it is highly likely that this script ran into a dead lock.
 --]]
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local player: Player = Players.LocalPlayer
 local playerScripts = player.PlayerScripts
 local BridgeNet2 = require(ReplicatedStorage.BridgeNet2)
 local UIHandler = require(playerScripts:WaitForChild("UIHandler"))
-local Object = require(ReplicatedStorage.Shared.Utilities.Object.Object)
-local StatsHandler = {}
-Object:Supersedes(StatsHandler)
+local ClientMutexSeq = require(playerScripts.ClientUtilities.ClientMutexSeq)
 
 --Instances
 local statsUIHandler: any = UIHandler.new("StatsUIHandler")
+local hungerMutex = ClientMutexSeq.new("HungerMutex")
+local thirstMutex = ClientMutexSeq.new("ThirstMutex")
 --Events
 local StatsDmgPlayer = BridgeNet2.ReferenceBridge("StatsDmgPlayer")
+local bindableEvents = ReplicatedFirst:WaitForChild("BindableEvents")
+local statBindEvents = bindableEvents:WaitForChild("Stats")
+local FeedPlayerEvent = statBindEvents:WaitForChild("FeedPlayer")
+local HydratePlayerEvent = statBindEvents:WaitForChild("HydratePlayer")
 
 local tasks: {thread} = {} --table of executing tasks
 local connections: {RBXScriptConnection} = {} --Table of conections
@@ -25,10 +32,10 @@ local statsConfig: {any} = {
 	--Handles config for stats
 	MaxFood = 100, --Max hunger of the player
 	MaxHydration = 100, --MaxHydration of the player
-	FdDeteriorationRate = 60, --time in seconds between when food stat gos down
+	FdDeteriorationRate = 5, --time in seconds between when food stat gos down
 	HydDeteriorationRate = 5, --time in seconds between when hydration stat gos down
-	FdDecrement = 20, --The amount that the food stat is decremented by every FdDeteriorationRate
-	HydDecrement = 20, --The amount that the hydration stat is decremented by every HydDeteriorationRate
+	FdDecrement = 5, --The amount that the food stat is decremented by every FdDeteriorationRate
+	HydDecrement = 5, --The amount that the hydration stat is decremented by every HydDeteriorationRate
 	StarveDmg = 20, --The damage done to a player every StarveDmgRate
 	StarveDmgRate = 5, --rate in seconds that damage is dealt during a starve
 	ThirstDmg = 20, --The damage done to a player every ThirstDmgRate
@@ -37,49 +44,9 @@ local statsConfig: {any} = {
 
 local stats: {number} = {
 	--The stat values
-	Food = statsConfig.MaxFood,
-	Hydration = statsConfig.MaxHydration,
+	Food = 50,
+	Hydration = 50,
 }
-
---[[
-Constructor for a StatsHandler instance
-	@param Name (string) the name of the instance
---]]
-function StatsHandler.new(Name: string)
-	local self = Object.new(Name)
-	setmetatable(self, StatsHandler)
-	return self
-end
-
---[[
-This function increases the players hunger stat by FoodRegen amount
-	if foodregen exceeds the config for max hunger then it is set to max hunger
-	@param FoodRegen (number) the amount to increase hunger stat by
---]]
-function StatsHandler:FeedPlayer(FoodRegen: number) : ()
-	--Up food stat by regen amount or max amount if greater than max amount
-	local newStat = stats.Food + FoodRegen
-	if newStat <= statsConfig.MaxFood then
-		stats.Food = newStat
-	else
-		stats.Food = statsConfig.MaxFood
-	end
-end
-
---[[
-This function increases the players hydration stat by HydrationRegen amount
-	if HydrationRegen exceeds the config for max hunger then it is set to max hydration
-	@param HydrationRegen (number) the amount to increase Hydration stat by
---]]
-function StatsHandler:HydratePlayer(HydrationRegen: number) : ()
-	--Up water stat by regen amount or max amount if greater than max amount
-	local newStat = stats.Hydration + HydrationRegen
-	if newStat <= statsConfig.MaxHydration then
-		stats.Hydration = newStat
-	else
-		stats.Hydration = statsConfig.MaxFood
-	end
-end
 
 --[[
 Starves the player until it is given food
@@ -88,10 +55,13 @@ local function Starve() : ()
 	tasks.StvTask = task.spawn(function()
 		--Damage NPC
 		while true do
+			hungerMutex:Lock()
 			if stats.Food > 0 then
 				tasks.StvTask = nil
+				hungerMutex:Unlock()
 				return--No longer starving
 			end
+			hungerMutex:Unlock()
 			--Tell server to damage player by given amount
             StatsDmgPlayer:Fire(statsConfig.StarveDmg)
 			task.wait(statsConfig.StarveDmgRate)
@@ -104,12 +74,14 @@ tasks.Hunger = task.spawn(function()
     local lastStat: number = 0
 	while true do
 		task.wait(statsConfig.FdDeteriorationRate) --Wait between decrements
+		hungerMutex:Lock()
 		local newStat: number = stats.Food - statsConfig.FdDecrement
 		if newStat < 0 then
 			newStat = 0 --Prevent negative stat
 		end
 
         if newStat == lastStat then
+			hungerMutex:Unlock()
             continue--Skip to next loop nothing new to do
         else
             --Update UI for new hunger stat
@@ -122,6 +94,7 @@ tasks.Hunger = task.spawn(function()
 			--Start damaging player and store task to cancel when given food
 			Starve()
 		end
+		hungerMutex:Unlock()
         lastStat = newStat --Update lastStat
 	end
 end)
@@ -133,10 +106,13 @@ local function Thirst() : ()
 	tasks.ThirstTask = task.spawn(function()
 		--Damage NPC
 		while true do
+			thirstMutex:Lock()
 			if stats.Hydration > 0 then
 				tasks.ThirstTask = nil
+				thirstMutex:Unlock()
 				return--No longer starving
 			end
+			thirstMutex:Unlock()
 			--Tell server to damage player by given amount
             StatsDmgPlayer:Fire(statsConfig.ThirstDmg)
 			task.wait(statsConfig.ThirstDmgRate)
@@ -149,12 +125,14 @@ tasks.Hydration = task.spawn(function()
     local lastStat: number = 0
 	while true do
 		task.wait(statsConfig.HydDeteriorationRate) --Wait between decrements
+		thirstMutex:Lock()
 		local newStat: number = stats.Hydration - statsConfig.HydDecrement
 		if newStat < 0 then
 			newStat = 0 --Prevent negative stat
 		end
 
         if newStat == lastStat then
+			thirstMutex:Unlock()
             continue--Skip to next loop nothing new to do
         else
             --Update UI for new Hydration stat
@@ -167,6 +145,7 @@ tasks.Hydration = task.spawn(function()
 			--Start damaging player because of thirst
 			Thirst()
 		end
+		thirstMutex:Unlock()
         lastStat = newStat --Update lastStat
 	end
 end)
@@ -191,4 +170,54 @@ tasks.Health = task.spawn(function()
 
 end)
 
-return StatsHandler
+--Handle stat events
+
+--[[
+This function increases the players hunger stat by FoodRegen amount
+	if foodregen exceeds the config for max hunger then it is set to max hunger
+	@param FoodRegen (number) the amount to increase hunger stat by
+--]]
+local function FeedPlayer(FoodRegen: number) : ()
+	--Up food stat by regen amount or max amount if greater than max amount
+	hungerMutex:Lock()
+	local newStat = stats.Food + FoodRegen
+	if newStat <= statsConfig.MaxFood then
+		stats.Food = newStat
+	else
+		stats.Food = statsConfig.MaxFood
+	end
+	--Update UI for new hunger stat
+	local newPrcnt: number = (newStat / statsConfig.MaxFood)
+	statsUIHandler:AdjustBarUI("Hunger", newPrcnt, false)
+	hungerMutex:Unlock()
+end
+
+--[[
+This function increases the players hydration stat by HydrationRegen amount
+	if HydrationRegen exceeds the config for max hunger then it is set to max hydration
+	@param HydrationRegen (number) the amount to increase Hydration stat by
+--]]
+local function HydratePlayer(HydrationRegen: number) : ()
+	--Up water stat by regen amount or max amount if greater than max amount
+	thirstMutex:Lock()
+	local newStat = stats.Hydration + HydrationRegen
+	if newStat <= statsConfig.MaxHydration then
+		stats.Hydration = newStat
+	else
+		stats.Hydration = statsConfig.MaxFood
+	end
+	--Update UI for new Hydration stat
+	local newPrcnt: number = (newStat / statsConfig.MaxHydration)
+	statsUIHandler:AdjustBarUI("Water", newPrcnt, false)
+	thirstMutex:Unlock()
+end
+
+FeedPlayerEvent.Event:Connect(function(FoodRegen)
+	print("Recieved Food Regen event!")
+	FeedPlayer(FoodRegen)
+end)
+
+HydratePlayerEvent.Event:Connect(function(HydrationRegen)
+	print("Recieved drink Regen event!")
+	HydratePlayer(HydrationRegen)
+end)
---
+++ src/client/StarterPlayer/StatsHandlerInterface.lua
@@ -0,0 +1,50 @@
+--[[
+This script is used to manipulate the StatsHandler in StarterCharacter
+Bindable events are used to allow for stat handling to destroy itself easily when the character dies and needs to be reset.
+Using bindable events allows for nothing to happen in the case that a character has not yet spawned or
+is in the process of respawning.
+--]]
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local Object = require(ReplicatedStorage.Shared.Utilities.Object.Object)
+local StatsHandlerInterface = {}
+Object:Supersedes(StatsHandlerInterface)
+
+--Events
+local bindableEvents = ReplicatedFirst:WaitForChild("BindableEvents")
+local statBindEvents = bindableEvents:WaitForChild("Stats")
+local FeedPlayerEvent = statBindEvents:WaitForChild("FeedPlayer")
+local HydratePlayerEvent = statBindEvents:WaitForChild("HydratePlayer")
+
+
+--[[
+Constructor for a StatsHandler instance
+	@param Name (string) the name of the instance
+--]]
+function StatsHandlerInterface.new(Name: string)
+	local self = Object.new(Name)
+	setmetatable(self, StatsHandlerInterface)
+	return self
+end
+
+--[[
+This function increases the players hunger stat by FoodRegen amount
+	if foodregen exceeds the config for max hunger then it is set to max hunger
+	@param FoodRegen (number) the amount to increase hunger stat by
+--]]
+function StatsHandlerInterface:FeedPlayer(FoodRegen: number) : ()
+	--Up food stat by regen amount or max amount if greater than max amount
+	FeedPlayerEvent:Fire(FoodRegen)
+end
+
+--[[
+This function increases the players hydration stat by HydrationRegen amount
+	if HydrationRegen exceeds the config for max hunger then it is set to max hydration
+	@param HydrationRegen (number) the amount to increase Hydration stat by
+--]]
+function StatsHandlerInterface:HydratePlayer(HydrationRegen: number) : ()
+	--Up water stat by regen amount or max amount if greater than max amount
+	HydratePlayerEvent:Fire(HydrationRegen)
+end
+
+return StatsHandlerInterface
