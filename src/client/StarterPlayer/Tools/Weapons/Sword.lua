f047dc1b1a76d1c73109aafa7225fcb10295d941
claytakiler <claytonhouser10@gmail.com>
2025-04-30 22:42:26 -0500

    Adding sword cool down system

--- src/client/StarterPlayer/Tools/Weapons/Sword.lua
+++ src/client/StarterPlayer/Tools/Weapons/Sword.lua
@@ -7,6 +7,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ExtType = require(ReplicatedStorage.Shared.ExtType)
 local PlayerUtilitiesObject = require(script.Parent.Parent.Parent.PlayerUtilities)
 local BridgeNet2 = require(ReplicatedStorage.BridgeNet2)
+local ClientMutexSeqObject = require(script.Parent.Parent.Parent.ClientUtilities.ClientMutexSeq)
 local Weapon = require(script.Parent.Weapon)
 local Sword = {}
 Weapon:Supersedes(Sword)
@@ -18,24 +19,25 @@ local DmgTarget: ExtType.Bridge = BridgeNet2.ReferenceBridge("DamageTargetCombat
 local playerUtilities = PlayerUtilitiesObject.new("PlayerUtilitiesSword")
 
 --Vars
-local swordCoolDown = 10
+local swordCoolDown = 3 --Time it takes for sword to cool down if less than swingAnim time
+local swordCount = 0 --The number of swords equipped so far
 
-function HandleHits(Self)
-    local hitbox = Self.__Hitbox
-    local damage = Self.__Damage
-    Self.__Connections["Hit"] = hitbox.OnHit:Connect(function(HitPart: BasePart, HitHum: Humanoid)
-        print("Hit a target!")
-        local character = HitHum.Parent
-        if playerUtilities:CanAttack(character) then
-            --tell server to damage player
-            print("CAN DAMAGE TARGET! AHHHHH")
-            local dmgTargetArgs = {
-                Damage = damage,
-                DmgHum = HitHum
-            }
-            DmgTarget:Fire(dmgTargetArgs)
-        end
-    end)
+local function HandleHits(Self)
+	local hitbox = Self.__Hitbox
+	local damage = Self.__Damage
+	Self.__Connections["Hit"] = hitbox.OnHit:Connect(function(HitPart: BasePart, HitHum: Humanoid)
+		print("Hit a target!")
+		local character = HitHum.Parent
+		if playerUtilities:CanAttack(character) then
+			--tell server to damage player
+			print("CAN DAMAGE TARGET! AHHHHH")
+			local dmgTargetArgs = {
+				Damage = damage,
+				DmgHum = HitHum,
+			}
+			DmgTarget:Fire(dmgTargetArgs)
+		end
+	end)
 end
 
 --[[
@@ -43,13 +45,13 @@ Given the swingAnim set for the sword, the cooldown time is comapred to the defa
     The max cooldown time allowed is returned
     @param SwingAnim (AnimationTrack) the swing animation track
 --]]
-local function MakeCoolDown(SwingAnim: AnimationTrack) : number
-    local animationTime = SwingAnim.Length
-    if animationTime <= swordCoolDown then
-        return swordCoolDown
-    else
-        return animationTime
-    end
+local function MakeCoolDown(SwingAnim: AnimationTrack): number
+	local animationTime = SwingAnim.Length
+	if animationTime <= swordCoolDown then
+		return swordCoolDown
+	else
+		return animationTime
+	end
 end
 
 --[[
@@ -57,18 +59,22 @@ The constructor for the Tools class
     @param Name (string) the name of this ObjectInstance
     @param PhysTool (Tool) the physical tool of the weapon being used
 --]]
-function Sword.new(Name: string, PhysTool: Tool) : ExtType.ObjectInstance?
-    local self = Weapon.new(Name, PhysTool)
+function Sword.new(Name: string, PhysTool: Tool): ExtType.ObjectInstance?
+	local self = Weapon.new(Name, PhysTool)
 	setmetatable(self, Sword)
-    --Set up swing anim priority
-    local swingAnim: AnimationTrack? = self.__Animations["Swing"]
-    if swingAnim == nil then
-        warn("Attempt to make sword instance but sword was missing Animation Swing")
-        return nil
-    else
-        swingAnim.Priority = Enum.AnimationPriority.Action2
-    end
-    self.__CoolDown = MakeCoolDown(swingAnim)
+	--Set up swing anim priority
+	local swingAnim: AnimationTrack? = self.__Animations["Swing"]
+	if swingAnim == nil then
+		warn("Attempt to make sword instance but sword was missing Animation Swing")
+		return nil
+	else
+		swingAnim.Priority = Enum.AnimationPriority.Action2
+	end
+	self.__CoolDownTime = MakeCoolDown(swingAnim)
+	self.__OnCoolDown = false --Indicates weather the tool is on cooldown
+	self.__CoolDownLockKey = "SwordCoolDown" .. swordCount
+	self.__CoolDownLock = ClientMutexSeqObject.new(self.__CoolDownLockKey)
+	swordCount = swordCount + 1
 	return self
 end
 
@@ -78,26 +84,49 @@ Ends the current attack swing.
     @param Animation (AnimationTrack) the animation track being played
     @param Hitbox (ExtType.RaycastHitbox) the RaycastHitboxV4 instance
 --]]
-local function EndAttack(Self: ExtType.ObjectInstance, Animation: AnimationTrack, Hitbox: ExtType.RaycastHitbox) : ()
-    Animation.Stopped:Wait()
-    Hitbox:HitStop()
+local function EndAttack(Self: ExtType.ObjectInstance, Animation: AnimationTrack, Hitbox: ExtType.RaycastHitbox): ()
+	Animation.Stopped:Wait()
+	Hitbox:HitStop()
+end
+
+local function CoolDown(Self)
+	Self.__Tasks["CoolDown"] = task.spawn(function()
+		Self.__CoolDownLock:Lock()
+		Self.__OnCoolDown = true
+		Self.__CoolDownLock:Unlock()
+		task.wait(Self.__CoolDownTime)
+		Self.__CoolDownLock:Lock()
+		Self.__OnCoolDown = false
+		Self.__CoolDownLock:Unlock()
+	end)
+end
+
+local function IsOnCoolDown(Self): boolean
+	Self.__CoolDownLock:Lock()
+	local coolDown = Self.__OnCoolDown
+	Self.__CoolDownLock:Unlock()
+	return coolDown
 end
 
 --[[
 Activates the tool given in the constructor
 --]]
-function Sword:Activate() : ()
-    print("SWORD WAS ACTIVATED!")
-    if not self.__Hitbox then
-        warn("Attempt to activate sword but sword was missing Hitbox for RaycastHitboxV4")
-        return
-    end
-    local swingAnim: AnimationTrack = self.__Animations["Swing"]
-    HandleHits(self)
-    local hitbox = self.__Hitbox
-    hitbox:HitStart()
-    swingAnim:Play()
-    EndAttack(self, swingAnim, hitbox)
+function Sword:Activate(): ()
+	print("SWORD WAS ACTIVATED!")
+	if not self.__Hitbox then
+		warn("Attempt to activate sword but sword was missing Hitbox for RaycastHitboxV4")
+		return
+	end
+	if IsOnCoolDown(self) then
+		return --on cool down so cant activate
+	end
+	CoolDown(self)--Handle cooldown
+	local swingAnim: AnimationTrack = self.__Animations["Swing"]
+	HandleHits(self)
+	local hitbox = self.__Hitbox
+	hitbox:HitStart()
+	swingAnim:Play()
+	EndAttack(self, swingAnim, hitbox)
 end
 
 --[[
@@ -107,9 +136,9 @@ Cleans up the given tool instance.
     Not using this function with a given instance may lead to both memory leaks
     and also undefined behaivore.
 --]]
-function Sword:DestroyInstance() : ()
-    --Clean up connections
-    self.__Connections["Hit"]:Disconnect()
+function Sword:DestroyInstance(): ()
+	--Clean up connections
+	self.__Connections["Hit"]:Disconnect()
 end
 
 return Sword
